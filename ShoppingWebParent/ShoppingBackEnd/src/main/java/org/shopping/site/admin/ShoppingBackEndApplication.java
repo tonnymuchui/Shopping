53b5396651a5238e45c3b2ae1ef12b8d2ee38969
tonnymuchui <tonykanyingah@gmail.com>
2024-02-27 04:07:54 +0300

    Add EntityScan

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/ShoppingBackEndApplication.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/ShoppingBackEndApplication.java
@@ -2,8 +2,11 @@ package org.shopping.site.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+@EntityScan({"org.shopping.entity", "org.shopping.site.admin.user"})
 public class ShoppingBackEndApplication {
 
     public static void main(String[] args) {
