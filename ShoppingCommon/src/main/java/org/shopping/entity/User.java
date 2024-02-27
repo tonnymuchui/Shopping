96addc4502df1ea4136463fae7373c172abe15f1
tonnymuchui <tonykanyingah@gmail.com>
2024-02-27 05:35:41 +0300

    Add User Entity

---
+++ ShoppingCommon/src/main/java/org/shopping/entity/User.java
@@ -0,0 +1,52 @@
+package org.shopping.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 64, nullable = false)
+    private String password;
+
+    @Column(name = "first_name", length = 45, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 45, nullable = false)
+    private String lastName;
+
+    @Column(length = 64)
+    private String photos;
+
+    private boolean enabled;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @ToString.Exclude
+    private Set<Role> role = new HashSet<>();
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public void addRole(Role role){
+        this.role.add(role);
+    }
+}
