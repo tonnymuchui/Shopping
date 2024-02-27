91aff8651d008f42c57284375466abfc2b8ede4c
tonnymuchui <tonykanyingah@gmail.com>
2024-02-27 05:35:51 +0300

    Add UserRepository

---
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserRepository.java
@@ -0,0 +1,9 @@
+package org.shopping.site.admin.user;
+
+import org.shopping.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
