bc1d31e45e5baf64f5b2abc388507016830f7d96
tonnymuchui <tonykanyingah@gmail.com>
2024-02-29 01:18:55 +0300

    Add save user & list users

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserService.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserService.java
@@ -1,7 +1,9 @@
 package org.shopping.site.admin.user;
 
+import org.shopping.entity.Role;
 import org.shopping.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +12,30 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
-
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
+    }
+    public List<Role> listAllRoles(){
+        return (List<Role>) roleRepository.findAll();
+    }
+    public void save(User user){
+        encodePassword(user);
+        userRepository.save(user);
+    }
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+    private void encodePassword(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
