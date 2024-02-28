80c1bb95b1a5363d1b37d880d1dbeb36e319f1f2
tonnymuchui <tonykanyingah@gmail.com>
2024-02-29 01:16:59 +0300

    Add configureHttpSecurity & passwordEncoder

---
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/security/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package org.shopping.site.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain configureHttpSecurity(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
