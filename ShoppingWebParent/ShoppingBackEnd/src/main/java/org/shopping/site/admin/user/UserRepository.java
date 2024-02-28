50c0f76e4a5d71797a9c8918bb3e6f3f212dde62
tonnymuchui <tonykanyingah@gmail.com>
2024-02-29 02:35:16 +0300

    Add getUserByEmail

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserRepository.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserRepository.java
@@ -1,9 +1,14 @@
 package org.shopping.site.admin.user;
 
 import org.shopping.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("select u from User u where u.email = :email")
+    public User getUserByEmail(@Param("email") String email);
 }
