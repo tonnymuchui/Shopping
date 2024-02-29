aba95c0d4cba9c91560b858264316844ab272ba9
tonnymuchui <tonykanyingah@gmail.com>
2024-03-01 02:33:27 +0300

    Add updateEnabledStatus

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserRepository.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserRepository.java
@@ -1,6 +1,9 @@
 package org.shopping.site.admin.user;
 
 import org.shopping.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +14,14 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     @Query("select u from User u where u.email = :email")
     public User getUserByEmail(@Param("email") String email);
+
+    public Long countById(Integer id);
+
+    @Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ',"
+            + " u.lastName) LIKE %?1%")
+    public Page<User> findAll(String keyword, Pageable pageable);
+
+    @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+    @Modifying
+    public void updateEnabledStatus(Integer id, boolean enabled);
 }
