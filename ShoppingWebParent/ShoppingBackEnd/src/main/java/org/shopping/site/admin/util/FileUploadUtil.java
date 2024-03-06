ef7cf9e383674d09f1e55134990937ac3aabd2c3
tonnymuchui <tonykanyingah@gmail.com>
2024-03-06 23:37:56 +0300

    Add clear&remove Dir

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/util/FileUploadUtil.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/util/FileUploadUtil.java
@@ -1,5 +1,8 @@
 package org.shopping.site.admin.util;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -10,6 +13,8 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class FileUploadUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+
     public static void saveFile(String uploadDir, String fileName,
                                 MultipartFile multipartFile) throws IOException {
         Path uploadPath = Paths.get(uploadDir);
@@ -25,5 +30,31 @@ public class FileUploadUtil {
             throw new IOException("Could not save file: " + fileName, ex);
         }
     }
+    public static void cleanDir(String dir) {
+        Path dirPath = Paths.get(dir);
 
+        try {
+            Files.list(dirPath).forEach(file -> {
+                if (!Files.isDirectory(file)) {
+                    try {
+                        Files.delete(file);
+                    } catch (IOException ex) {
+                        LOGGER.error("Could not delete file: " + file);
+                    }
+                }
+            });
+        } catch (IOException ex) {
+            LOGGER.error("Could not list directory: " + dirPath);
+        }
+    }
+    public static void removeDir(String dir) {
+        cleanDir(dir);
+
+        try {
+            Files.delete(Paths.get(dir));
+        } catch (IOException e) {
+            LOGGER.error("Could not remove directory: " + dir);
+        }
+
+    }
 }
