9e7eea5d1a16890c1b1909fd3c1ecc9a83df950c
tonnymuchui <tonykanyingah@gmail.com>
2024-03-08 19:11:40 +0300

    Add firstPageTest&testSearchUsers

--- ShoppingWebParent/ShoppingBackEnd/src/test/java/org/shopping/site/admin/user/UserRepositoryTests.java
+++ ShoppingWebParent/ShoppingBackEnd/src/test/java/org/shopping/site/admin/user/UserRepositoryTests.java
@@ -7,7 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -87,5 +92,32 @@ public class UserRepositoryTests {
         String email = "test22@gmail.com";
         User user = userRepository.getUserByEmail(email);
         assertThat(user).isNotNull();
+    }
+    @Test
+    public void firstPageTest(){
+        int pageNumber = 0;
+        int pageSize = 4;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<User> page = userRepository.findAll(pageable);
+
+        List<User> listUsers = page.getContent();
+        listUsers.forEach(System.out::println);
+        assertThat(listUsers.size()).isEqualTo(pageSize);
+    }
+    @Test
+    public void testSearchUsers() {
+        String keyword = "test";
+
+        int pageNumber = 0;
+        int pageSize = 4;
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<User> page = userRepository.findAll(keyword, pageable);
+
+        List<User> listUsers = page.getContent();
+
+        listUsers.forEach(System.out::println);
+
+        assertThat(listUsers.size()).isGreaterThan(0);
     }
 }
