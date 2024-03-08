e10f0553bda11a695e5f961d1fb1b23abca98ce4
tonnymuchui <tonykanyingah@gmail.com>
2024-03-08 19:11:05 +0300

    Update findAll Users Paging

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserRepository.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/user/UserRepository.java
@@ -3,14 +3,15 @@ package org.shopping.site.admin.user;
 import org.shopping.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("select u from User u where u.email = :email")
     public User getUserByEmail(@Param("email") String email);
