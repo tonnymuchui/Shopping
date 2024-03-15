45177124e911ad1b1486bdeb9b2b7db89238b1dc
tonnymuchui <tonykanyingah@gmail.com>
2024-03-16 00:39:58 +0300

    Add CategoryRepository

---
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/category/CategoryRepository.java
@@ -0,0 +1,10 @@
+package org.shopping.site.admin.category;
+
+import org.shopping.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
