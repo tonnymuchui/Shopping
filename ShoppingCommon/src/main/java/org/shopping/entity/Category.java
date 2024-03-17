dfdadf413a26cb907eb4df552c0943159732b821
tonnymuchui <tonykanyingah@gmail.com>
2024-03-17 19:03:30 +0300

    Add Category Entity

--- ShoppingCommon/src/main/java/org/shopping/entity/Category.java
+++ ShoppingCommon/src/main/java/org/shopping/entity/Category.java
@@ -33,18 +33,63 @@ public class Category {
     @ToString.Exclude
     private Set<Category> children = new HashSet<>();
 
+    @Setter
+    @Getter
+    @Transient
+    private boolean hasChildren;
+
     public Category(Integer id) {
         super();
         this.id = id;
     }
     public Category(String name) {
-        super();
         this.name = name;
         this.alias = name;
         this.image = "default.png";
     }
+
     public Category(String name, Category parent) {
         this(name);
         this.parent = parent;
+    }
+
+    public Category(Integer id, String name, String alias) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.alias = alias;
+    }
+    public static Category copyIdAndName(Category category) {
+        Category copyCategory = new Category();
+        copyCategory.setId(category.getId());
+        copyCategory.setName(category.getName());
+
+        return copyCategory;
+    }
+
+    public static Category copyIdAndName(Integer id, String name) {
+        Category copyCategory = new Category();
+        copyCategory.setId(id);
+        copyCategory.setName(name);
+
+        return copyCategory;
+    }
+    public static Category copyFull(Category category) {
+        Category copyCategory = new Category();
+        copyCategory.setId(category.getId());
+        copyCategory.setName(category.getName());
+        copyCategory.setImage(category.getImage());
+        copyCategory.setAlias(category.getAlias());
+        copyCategory.setEnabled(category.isEnabled());
+        copyCategory.setHasChildren(!category.getChildren().isEmpty());
+
+        return copyCategory;
+    }
+
+    public static Category copyFull(Category category, String name) {
+        Category copyCategory = Category.copyFull(category);
+        copyCategory.setName(name);
+
+        return copyCategory;
     }
 }
