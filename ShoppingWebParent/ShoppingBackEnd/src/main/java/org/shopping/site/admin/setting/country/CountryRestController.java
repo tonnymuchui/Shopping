70fbe4f0165303375383e6a2e04b44e041aff47a
tonnymuchui <tonykanyingah@gmail.com>
2024-07-08 13:19:12 +0300

    Add CountryRestController

---
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/setting/country/CountryRestController.java
@@ -0,0 +1,30 @@
+package org.shopping.site.admin.setting.country;
+
+import org.shopping.entity.setting.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CountryRestController {
+
+    @Autowired
+    private CountryRepository repo;
+
+    @GetMapping("/countries/list")
+    public List<Country> listAll() {
+        return repo.findAllByOrderByNameAsc();
+    }
+
+    @PostMapping("/countries/save")
+    public String save(@RequestBody Country country) {
+        Country savedCountry = repo.save(country);
+        return String.valueOf(savedCountry.getId());
+    }
+
+    @DeleteMapping("/countries/delete/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        repo.deleteById(id);
+    }
+}
