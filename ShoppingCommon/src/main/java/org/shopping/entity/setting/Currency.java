75826b8bc90013f74f43f01faf1b08d6b5268512
tonnymuchui <tonykanyingah@gmail.com>
2024-07-08 13:18:01 +0300

    Add Currency

---
+++ ShoppingCommon/src/main/java/org/shopping/entity/setting/Currency.java
@@ -0,0 +1,62 @@
+package org.shopping.entity.setting;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.shopping.entity.IdBasedEntity;
+
+@Entity
+@Table(name = "currencies")
+public class Currency extends IdBasedEntity {
+
+    @Column(nullable = false, length = 64)
+    private String name;
+
+    @Column(nullable = false, length = 3)
+    private String symbol;
+
+    @Column(nullable = false, length = 4)
+    private String code;
+
+    public Currency() {
+
+    }
+
+    public Currency(String name, String symbol, String code) {
+        super();
+        this.name = name;
+        this.symbol = symbol;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + code + " - " + symbol;
+    }
+
+
+}
