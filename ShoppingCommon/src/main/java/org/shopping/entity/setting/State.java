37a272162dffd3e324e74093c7f0dabb52509ae7
tonnymuchui <tonykanyingah@gmail.com>
2024-07-08 13:17:37 +0300

    Add State Entity

---
+++ ShoppingCommon/src/main/java/org/shopping/entity/setting/State.java
@@ -0,0 +1,48 @@
+package org.shopping.entity.setting;
+
+import jakarta.persistence.*;
+import org.shopping.entity.IdBasedEntity;
+
+@Entity
+@Table(name = "states")
+public class State extends IdBasedEntity {
+
+    @Column(nullable = false, length = 45)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    public State() {
+
+    }
+
+    public State(String name, Country country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "State [id=" + id + ", name=" + name + "]";
+    }
+
+
+}
