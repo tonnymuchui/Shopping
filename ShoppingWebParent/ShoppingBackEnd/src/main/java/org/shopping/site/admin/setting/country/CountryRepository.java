265ba56e33ba9df2c78037e01e61fbd947b25796
tonnymuchui <tonykanyingah@gmail.com>
2024-07-08 13:18:33 +0300

    Add CountryRepository

---
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/org/shopping/site/admin/setting/country/CountryRepository.java
@@ -0,0 +1,10 @@
+package org.shopping.site.admin.setting.country;
+
+import org.shopping.entity.setting.Country;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CountryRepository extends CrudRepository<Country, Integer> {
+    public List<Country> findAllByOrderByNameAsc();
+}
